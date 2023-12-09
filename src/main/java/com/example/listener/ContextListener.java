09ba8ead7d7e68516fbdb60e36f7467ddda4f911
boris <baizenbe@gmail.com>
2023-12-09 17:59:28 +0600

    serveee

--- src/main/java/com/example/listener/ContextListener.java
+++ src/main/java/com/example/listener/ContextListener.java
@@ -1,5 +1,24 @@
 package com.example.listener;
 
-public class ContextListener {
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import java.time.LocalDateTime;
+
+@WebListener
+public class ContextListener implements ServletContextListener {
+    public void contextInitialized(ServletContextEvent sce){
+        ServletContext context = sce.getServletContext();
+        LocalDateTime servletTimeInit = LocalDateTime.now();
+        context.setAttribute("servletTimeInit", servletTimeInit);
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+
+    }
+
     //write your code here!
 }
--- src/main/java/com/example/servlet/LoginServlet.java
+++ src/main/java/com/example/servlet/LoginServlet.java
@@ -1,5 +1,40 @@
 package com.example.servlet;
 
-public class LoginServlet {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import com.example.Users;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession(false);
+        if(session !=null && session.getAttribute("user")!= null){
+            resp.sendRedirect(req.getContextPath() + "/login.jsp");
+        } else {
+            resp.sendRedirect(req.getContextPath() + "/user/hello.jsp");
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String login = req.getParameter("login");
+        String password = req.getParameter("password");
+        Users instance = Users.getInstance();
+
+        if(instance.getUsers().contains(login)  && password != null && !password.isEmpty()){
+            HttpSession session = req.getSession();
+            session.setAttribute("user", login);
+            resp.sendRedirect(req.getContextPath() + "/user/hello.jsp");
+        } else {
+            req.getRequestDispatcher("/login.jsp").forward(req,resp);
+        }
+    }
+
     //write your code here!
 }
